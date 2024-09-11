fd2034486cccd215a2f0fd8ea8e2f5cd478c1b7a
Sermio21 <62938846+Sergio-Sousa-Sermio21@users.noreply.github.com>
2024-09-11 17:04:09 +0100

    Adicionando os files do site da aparcour

---
+++ aparcour/js/button.js
@@ -0,0 +1,3 @@
+document.querySelector('.js-home-button').addEventListener("click", () => window.location.href = "index.html");
+document.querySelector(".js-about-button").addEventListener("click", () => window.location.href = "about.html");
+document.querySelector(".js-home-button").addEventListener("click", () => window.location.href = "index.html");
