9a6b914deb25b5bd8cf1ad8d5642d9efd2105089
Sermio21 <62938846+Sergio-Sousa-Sermio21@users.noreply.github.com>
2024-10-29 16:55:22 +0000

    Added footer(needs more info), about button working

--- aparcour/js/button.js
+++ aparcour/js/button.js
@@ -1,4 +1,4 @@
 document.querySelector('.js-home-button').addEventListener("click", () => window.location.href = "index.html");
-document.querySelector(".js-about-button").addEventListener("click", () => window.location.href = "about.html");
+document.querySelector(".js-about-button").addEventListener("click", () => document.getElementById("about").scrollIntoView({ behavior: 'smooth' }));
 document.querySelector(".js-info-button").addEventListener("click", () => window.location.href = "local.html");
 document.querySelector(".js-register-button").addEventListener("click", () => window.open('https://docs.google.com/forms/d/e/1FAIpQLSd4xyxp3TCe1OJ0YBv7mRuyWCl4DCWe8NC6JrSygfE-Z3flIA/viewform' , '_blank'));
